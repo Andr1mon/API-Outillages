3e6773ced3c1477f33f7a3362d14c8f47f70bc36
Andr1mon <andreiamburwev@mail.ru>
2023-12-05 15:43:01 +0100

    Bug fix

--- src/main/java/tp/ensim/TPINFO2/model/Address.java
+++ src/main/java/tp/ensim/TPINFO2/model/Address.java
@@ -13,5 +13,37 @@ public class Address {
     private Date creation;
     private String content;
     private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreation() {
+        return creation;
+    }
+
+    public void setCreation(Date creation) {
+        this.creation = creation;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
 
